ad6709dc3bb57aba79bd8f6c82c3b1d6f3472468
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-12-13 10:20:43 +0200

    feat(#9): Configure tailwindcss for manual theme change by class or data-mode
    Signed-off-by: Kaloyan Doychinov <kaloyan@kaloyan.tech>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    darkMode: ['class', '[data-mode="dark"]'],
     fontFamily: {
       inter: "'Inter', sans-serif",
     },
@@ -27,40 +24,17 @@ const config: Config = {
       'text-secondary': 'var(--text-secondary)',
       'text-important': 'var(--text-important)',
     },
-  },
-  extend: {
-    boxShadow: {
-      // TODO: add box shadows according to the design in figma
-    },
-    spacing: {
-      '1/2': '50%',
-      '1/3': '33.333333%',
-      '2/3': '66.666667%',
-      '1/4': '25%',
-      '2/4': '50%',
-      '3/4': '75%',
-      '1/5': '20%',
-      '2/5': '40%',
-      '3/5': '60%',
-      '4/5': '80%',
-      '1/6': '16.666667%',
-      '2/6': '33.333333%',
-      '3/6': '50%',
-      '4/6': '66.666667%',
-      '5/6': '83.333333%',
-      '1/12': '8.333333%',
-      '2/12': '16.666667%',
-      '3/12': '25%',
-      '4/12': '33.333333%',
-      '5/12': '41.666667%',
-      '6/12': '50%',
-      '7/12': '58.333333%',
-      '8/12': '66.666667%',
-      '9/12': '75%',
-      '10/12': '83.333333%',
-      '11/12': '91.666667%',
+    extend: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      backgroundImage: (theme) => ({
+        gradient: 'linear-gradient(99deg, #28EDA3 31.45%, #06A7F5 68.87%)',
+      }),
+      boxShadow: {
+        // TODO: add box shadows according to the design in figma
+      },
     },
   },
+
   plugins: [],
 };
 export default config;
