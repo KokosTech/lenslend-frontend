dd16111623cb86c526cb2856feaa44cf9729ea3f
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-01-09 12:35:05 +0200

    fix(#3): fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,8 +1,11 @@
 import { AxiosResponse } from 'axios';
 import { axiosInstance } from '@/configs/axios';
+import { API_URL } from '@/configs/api';
 
 const fetcher = async <DataType>(url: string): Promise<DataType> => {
-  const response: AxiosResponse<DataType> = await axiosInstance.get(url);
+  const response: AxiosResponse<DataType> = await axiosInstance.get(
+    API_URL + url,
+  );
   return response.data;
 };
 
