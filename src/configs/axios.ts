94c90fe91373b43e55201115a508c6901041498f
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-01-19 06:55:00 +0200

    feat(#31): fix axios?

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -4,5 +4,8 @@ import { API_URL } from '@/configs/api';
 // Create a type for the Axios instance with the specified configuration options
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL,
-  // Add other configuration options as needed
+  // make it work for server actions
+  headers: {
+    cookie: typeof window === 'undefined' ? undefined : document.cookie,
+  },
 });
