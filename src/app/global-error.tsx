d0eb9ee70597619788be83e9cdc1a31386ad6bd9
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-02-04 21:54:36 +0200

    add: visibility and error handling to listing and comment pages

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import React from 'react';
+import { ThemeProvider } from '@/context/ThemeProvider';
+
 export default function GlobalError({
   error,
   reset,
@@ -10,10 +13,26 @@ export default function GlobalError({
   console.error(error);
 
   return (
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+    <html className='dark'>
+      <body className={'flex flex-row bg-background text-text'}>
+        <ThemeProvider>
+          <main className='min-h-screen w-full px-4 pb-4 pt-24 md:py-4'>
+            <div className='flex h-screen w-full flex-col items-center justify-center'>
+              <h2 className='bg-gradient bg-clip-text p-2 text-5xl font-black text-transparent'>
+                Something went wrong
+              </h2>
+              <h2 className='bg-gradient bg-clip-text p-2 text-5xl font-black text-transparent'>
+                Нещо се обърка
+              </h2>
+              <button
+                className='text-white mt-8 rounded-md bg-primary px-5 py-2 transition-all hover:scale-105'
+                onClick={() => reset()}
+              >
+                Try again / Опитай пак
+              </button>
+            </div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/fetch/listing.fetch.ts
+++ src/fetch/listing.fetch.ts
@@ -1,14 +1,33 @@
+/* eslint-disable indent */
+
 import { API_URL } from '@/configs/api';
 import { FullListingResponse } from '@/types/data/listing.type';
+import { getAuth } from '@/actions/auth';
+import {
+  HTTPForbiddenException,
+  HTTPUnauthorizedException,
+} from '@/errors/HTTPExceptions';
 
 const getListing = async (uuid: string) => {
+  const auth = await getAuth();
+
   const response = await fetch(`${API_URL}/listing/${uuid}`, {
     cache: 'no-cache',
+    headers: auth
+      ? {
+          Authorization: auth,
+        }
+      : {},
   });
 
   if (!response.ok) {
-    if (response.status === 404) {
-      return null;
+    switch (response.status) {
+      case 401:
+        throw new HTTPUnauthorizedException();
+      case 403:
+        throw new HTTPForbiddenException();
+      case 404:
+        return null;
     }
 
     throw new Error(`Couldn't fetch listing ${response.status}`);
