e8d9db664aa44ce17946d8c9910197e7b81eec92
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-02-11 17:22:27 +0200

    feat(#16): add pagination to places grid

--- src/partials/grid/places.grid.tsx
+++ src/partials/grid/places.grid.tsx
@@ -1,35 +1,92 @@
 import PlaceCard from '@/components/common/cards/place.card';
-import { API_URL } from '@/configs/api';
+import { paginatedFetch } from '@/utils/paginated-fetch';
+import CategoryTitle from '@/components/common/cateogry-title';
+import { PaginatedResponse } from '@/types/paginated-response.type';
 
-const getPlaces = async () => {
-  const response = await fetch(`${API_URL}/place?format=card`);
-  if (!response.ok) {
-    throw new Error(response.statusText);
+const getPlaces = async (category?: string, username?: string) => {
+  try {
+    const data = await paginatedFetch<{
+      uuid: string;
+      name: string;
+      lat: number;
+      lng: number;
+      thumbnail: {
+        url: string;
+        alt: string;
+      };
+      rating: number;
+    }>(
+      `${username ? `/user/${username}` : ''}/place?format=card${category ? `&category=${category}` : ''}`,
+      1,
+      4,
+      {
+        next: {
+          revalidate: 60,
+        },
+        cache: 'no-cache',
+      },
+    );
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
+};
 
-  const data = (await response.json()) as {
+const PlacesGrid = async ({
+  title,
+  url,
+  username,
+  category,
+}: {
+  title?: string;
+  url?: string;
+  username?: string;
+  category?: string;
+}) => {
+  const placesData: PaginatedResponse<{
     uuid: string;
     name: string;
     lat: number;
     lng: number;
-    images: {
+    thumbnail: {
       url: string;
-    }[];
-  }[];
+      alt: string;
+    };
+    rating: number;
+  }> | null = await getPlaces(category, username);
 
-  return data;
-};
+  if (!placesData) {
+    return <div>Failed to load places</div>;
+  }
 
-const PlacesGrid = async () => {
-  const places = await getPlaces();
+  const { data: places, totalCount } = placesData;
 
-  if (!places) return null;
+  if (!places) {
+    return <div>No places found</div>;
+  }
+
+  if (totalCount === 0) {
+    return null;
+  }
 
   return (
-    <div className='grid w-full grid-cols-1 gap-4 p-2 lg:grid-cols-2 2xl:grid-cols-3'>
-      {places.map((place) => (
-        <PlaceCard key={place.uuid} place={place} />
-      ))}
+    <div className='flex w-full flex-col gap-4'>
+      {title && (
+        <CategoryTitle title={title} url={totalCount < 4 ? undefined : url} />
+      )}
+      <div
+        className={`grid w-full grid-cols-1 gap-4 overflow-hidden ${
+          username
+            ? '2xl:grid-cols-2 min-[1800px]:grid-cols-3 min-[1800px]:grid-rows-[1fr_0_0] min-[1800px]:gap-y-0'
+            : 'lg:grid-cols-2 2xl:grid-cols-3 2xl:grid-rows-[1fr_0_0] 2xl:gap-y-0'
+        }`}
+      >
+        {places.map((place) => (
+          <PlaceCard key={place.uuid} place={place} />
+        ))}
+      </div>
     </div>
   );
 };
