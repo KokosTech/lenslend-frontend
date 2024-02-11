0d6894e1a57b83ce303284d5fa3f16799da8f0b0
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-02-11 17:25:57 +0200

    feat(#16): add view all reviews

--- src/app/[locale]/places/[uuid]/reviews/page.tsx
+++ src/app/[locale]/places/[uuid]/reviews/page.tsx
@@ -1,49 +1,80 @@
-import { API_URL } from '@/configs/api';
-import { Review as ReviewType } from '@/types/data/place.type';
-import { ReviewServer } from '@/components/place/review';
 import BackButton from '@/components/common/buttons/backButton';
+import { ReviewServer } from '@/components/place/review';
+import { Review as ReviewType } from '@/types/data/place.type';
 import { unstable_setRequestLocale } from 'next-intl/server';
+import { paginatedFetch } from '@/utils/paginated-fetch';
+import { PaginatedResponse } from '@/types/paginated-response.type';
+import { notFound } from 'next/navigation';
+import PageOptions from '@/partials/common/pageOptions';
 
-const getReviews = async (uuid: string) => {
-  const response = await fetch(`${API_URL}/place/${uuid}/review`, {
-    cache: 'no-cache',
-  });
+const getReviews = async (
+  uuid: string,
+  page: number = 1,
+  limit: number = 12,
+) => {
+  const response = await paginatedFetch<ReviewType>(
+    `/place/${uuid}/review`,
+    page,
+    limit,
+    {
+      cache: 'no-cache',
+    },
+  );
 
-  if (!response.ok) {
-    if (response.status === 404) {
-      return null;
-    }
-
-    throw new Error(`Couldn't fetch reviews ${response.status}`);
+  if (!response) {
+    return null;
   }
 
-  return (await response.json()) as ReviewType[];
+  return response;
 };
 
 const ReviewsPage = async ({
   params: { uuid, locale },
+  searchParams: { page = 1, limit = 12 },
 }: {
   params: { uuid: string; locale: string };
+  searchParams: { page: number; limit: number };
 }) => {
   unstable_setRequestLocale(locale);
 
-  const reviews: ReviewType[] | null = await getReviews(uuid);
+  if (Number.isNaN(Number(page)) || Number.isNaN(Number(limit))) {
+    return (
+      <div>
+        <h1>Invalid page or limit</h1>
+      </div>
+    );
+  }
 
-  if (!reviews) {
-    return <p>No reviews</p>;
+  const reviewsData: PaginatedResponse<ReviewType> | null = await getReviews(
+    uuid,
+    page,
+    limit,
+  );
+
+  if (!reviewsData || !uuid) {
+    return notFound();
+  }
+
+  const { data: reviews, totalCount: totalItems } = reviewsData;
+
+  if (totalItems === 0) {
+    return (
+      <div>
+        <h1>No reviews</h1>
+      </div>
+    );
   }
 
   return (
-    <div className='flex justify-center'>
-      <div className='flex max-w-2xl flex-col items-center gap-4'>
-        <div className='flex items-center justify-center gap-4'>
-          <BackButton className='' />
-          <h4 className='text-2xl font-semibold text-text'>Reviews</h4>
-        </div>
-        {reviews.map((review) => (
-          <ReviewServer {...review} key={review.uuid} />
-        ))}
+    <div className='flex w-full max-w-2xl flex-col items-center gap-4'>
+      <div className='flex items-center justify-center gap-4'>
+        <BackButton className='' />
+        <h4 className='text-2xl font-semibold text-text'>Reviews</h4>
       </div>
+      {reviews.map((review) => (
+        <ReviewServer {...review} key={review.uuid} />
+      ))}
+      <PageOptions page={page} limit={limit} totalItems={totalItems} />
     </div>
   );
 };
